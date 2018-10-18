333dc458268fff5de924a1d37a59aefcd8ff9e19
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-18 00:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 
 var height = "74"
-var name = "Susan"
+var name = "Joe"
 
 // Don't worry about this
 module.exports = { name, height
 }
 
-if (name === "Susan") {
-  console.log("The name is correct")
-}
-else {
-  console.log("Expected the name to be Susan")
-}
+n
